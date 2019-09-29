5d64b28afeda2d2bb26d69ea8a8ee65fafdb0b00
isidoreduc <isidoreducasse79@gmail.com>
2019-09-29 15:30:07 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "../css/error.module.css"
+import Hero from "../components/hero"
+import Banner from "../components/banner"
+import { Link } from "gatsby"
 
 const Error = () => {
   return (
     <Layout>
-      <div>404 Error</div>
+      <header className={styles.error}>
+        <Banner title="Ooops, 404, cul de sac.">
+          <Link to="/" className='btn-white'>
+            back is were home is
+          </Link>
+        </Banner>
+      </header>
     </Layout>
   )
 }
