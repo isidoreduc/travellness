043d599633bcba64de62f86c939c338075184581
isidoreduc <isidoreducasse79@gmail.com>
2019-09-29 17:39:36 +0100

    about us component - using styled components

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,36 @@
 import React from "react"
 import Title from "./title"
+import styles from "../css/about.module.css"
+import img from "../images/defaultBcg.jpeg"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
-    <div>
+    <section className={styles.about}>
       <Title firstWord="about" rest="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the earth</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            molestias inventore dolore dignissimos consectetur architecto?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
+            dicta sed quaerat quidem mollitia possimus similique laborum nam
+            excepturi omnis.
+          </p>
+          <Link className="btn-primary" to="/tours">
+            read more
+          </Link>
+        </article>
+      </div>
+    </section>
   )
 }
 
