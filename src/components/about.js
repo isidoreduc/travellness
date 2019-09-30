91a9b1d611937f99dac69ae7f5f46f9123c84a8e
isidoreduc <isidoreducasse79@gmail.com>
2019-09-30 18:41:57 +0100

    getting abou image via graphql image optimizer

--- src/components/about.js
+++ src/components/about.js
@@ -1,17 +1,18 @@
-import React from "react"
-import Title from "./title"
-import styles from "../css/about.module.css"
-import img from "../images/defaultBcg.jpeg"
-import { Link } from "gatsby"
+import React from 'react';
+import Title from './title';
+import styles from '../css/about.module.css';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 const About = () => {
+  const { aboutImage } = useStaticQuery(data);
   return (
     <section className={styles.about}>
       <Title title="about" subtitle="us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="" />
+            <Img fluid={aboutImage.childImageSharp.fluid} />
           </div>
         </article>
         <article className={styles.aboutInfo}>
@@ -31,7 +32,19 @@ const About = () => {
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+const data = graphql`
+  {
+    aboutImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+export default About;
