b075beaa94989a662ca33e8191a1371f34b6ea36
isidoreduc <isidoreducasse79@gmail.com>
2019-10-01 16:14:59 +0100

    getting home background image via graphql image optimizer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
-import React from "react"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Banner from "../components/banner"
-import { Link } from "gatsby"
-import About from "../components/about"
-import OurServices from "../components/ourServices"
+import React from 'react';
+import Layout from '../components/layout';
+import StyledHero from '../components/styledHero';
+import Banner from '../components/banner';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import About from '../components/about';
+import OurServices from '../components/ourServices';
 
 export default () => {
+  const { homeImage } = useStaticQuery(data);
+
   return (
     <Layout>
-      <Hero>
+      <StyledHero img={homeImage.childImageSharp.fluid} home>
         <Banner
           title="travel well. trawell"
           info="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, quibusdam?"
@@ -18,10 +20,22 @@ export default () => {
             explore tours
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
 
       <About />
       <OurServices />
     </Layout>
-  )
-}
+  );
+};
+
+const data = graphql`
+  {
+    homeImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
