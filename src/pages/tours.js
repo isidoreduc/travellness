79a0565df222c00707d24bc984adc4afcd54d820
isidoreduc <isidoreducasse79@gmail.com>
2019-10-01 17:16:22 +0100

    set images for all pages

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,12 +1,27 @@
-import React from "react"
-import Layout from "../components/layout"
-import Button from "../examples-reference/button"
-const tours = () => {
+import React from 'react';
+import Layout from '../components/layout';
+import Button from '../examples-reference/button';
+import StyledHero from '../components/styledHero';
+
+// getting image using props, as opposed to hook useStaticQuery
+const Tours = ({ data }) => {
   return (
     <Layout>
-      <div>tours</div>
+      <StyledHero img={data.homeImage.childImageSharp.fluid}></StyledHero>
     </Layout>
-  )
-}
+  );
+};
 
-export default tours
+export const query = graphql`
+  {
+    homeImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+export default Tours;
